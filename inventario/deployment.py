3edcf5abcce26f4ddc0c715d8fe414d8cd04aae8
davidvegemeal1 <mezaazul24@gmail.com>
2023-12-19 00:27:28 -0600

    agregacollectstatic

--- inventario/deployment.py
+++ inventario/deployment.py
@@ -19,16 +19,9 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Ruta del sistema de archivos donde se almacenarán los archivos estáticos recopilados
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Otros ajustes relacionados con archivos estáticos (opcional)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 host = os.getenv('AZURE_MYSQL_HOST')
